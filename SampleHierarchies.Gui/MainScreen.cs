1e4d4c2b1bce053e8916d0ba5406c3c299c19e9c
KnownSloth <127199757+KnownSloth@users.noreply.github.com>
2023-09-15 09:54:47 +0200

    Add project files.

---
+++ SampleHierarchies.Gui/MainScreen.cs
@@ -0,0 +1,103 @@
+﻿using SampleHierarchies.Enums;
+using SampleHierarchies.Interfaces.Services;
+
+namespace SampleHierarchies.Gui;
+
+/// <summary>
+/// Application main screen.
+/// </summary>
+public sealed class MainScreen : Screen
+{
+    #region Properties And Ctor
+
+    /// <summary>
+    /// Data service.
+    /// </summary>
+    private IDataService _dataService;
+
+    /// <summary>
+    /// Setting service.
+    /// </summary>
+    private ISettingsService _settingService;
+
+    /// <summary>
+    /// Animals screen.
+    /// </summary>
+    private AnimalsScreen _animalsScreen;
+    /// <summary>
+    /// Setting Screen.
+    /// </summary>
+    private SettingScreen _settingScreen;
+
+    /// <summary>
+    /// Ctor.
+    /// </summary>
+    /// <param name="dataService">Data service reference</param>
+    /// <param name="settingaService">Settingservice reference</param>
+    /// <param name="animalsScreen">Animals screen</param>
+    /// <param name="settingScreen">Setting Screen</param>
+    public MainScreen(
+        IDataService dataService,
+    ISettingsService settingService,
+        AnimalsScreen animalsScreen,
+        SettingScreen settingScreen)
+    {
+        _dataService = dataService;
+        _settingService = settingService;
+        _animalsScreen = animalsScreen;
+        _settingScreen = settingScreen;
+    }
+
+    #endregion Properties And Ctor
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public override void Show()
+    {
+        while (true)
+        {
+            Console.BackgroundColor = _settingService.settings.MainScreen;
+            Console.WriteLine();
+            Console.WriteLine("Your available choices are:");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Animals");
+            Console.WriteLine("2. Create a new settings");
+            Console.Write("Please enter your choice: ");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            string? choiceAsString = Console.ReadLine();
+
+            // Validate choice
+            try
+            {
+                if (choiceAsString is null)
+                {
+                    throw new ArgumentNullException(nameof(choiceAsString));
+                }
+
+                MainScreenChoices choice = (MainScreenChoices)Int32.Parse(choiceAsString);
+                switch (choice)
+                {
+                    case MainScreenChoices.Animals:
+                        _animalsScreen.Show();
+                        break;
+
+                    case MainScreenChoices.Settings:
+                        _settingScreen.Show();
+                        break;
+
+                    case MainScreenChoices.Exit:
+                        Console.WriteLine("Goodbye.");
+                        return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    #endregion // Public Methods
+}
---
+++ SampleHierarchies.Gui/MammalsScreen.cs
@@ -0,0 +1,98 @@
+﻿using SampleHierarchies.Data.Mammals;
+using SampleHierarchies.Enums;
+using SampleHierarchies.Interfaces.Services;
+using SampleHierarchies.Services;
+
+namespace SampleHierarchies.Gui;
+
+/// <summary>
+/// Mammals main screen.
+/// </summary>
+public sealed class MammalsScreen : Screen
+{
+    #region Properties And Ctor
+
+    /// <summary>
+    /// Animals screen.
+    /// </summary>
+    private ISettingsService _settingsService;
+    private DogsScreen _dogsScreen;
+    private AfricanelephantsScreen _africanelephantsScreen;
+    private PolarBearsScreen _polarBearsScreen;
+    private ChimpanzeesScreen _chimmanzee;
+
+    /// <summary>
+    /// Ctor.
+    /// </summary>
+    /// <param name="settingsService">Settings service reference</param>
+    /// <param name="dataService">Data service reference</param>
+    /// <param name="dogsScreen">Dogs screen</param>
+    public MammalsScreen(ISettingsService settingsService, DogsScreen dogsScreen, AfricanelephantsScreen africanelephantsScreen, PolarBearsScreen polarBearsScreen, ChimpanzeesScreen chimpanzeesScreen)
+    {
+        _settingsService = settingsService;
+        _dogsScreen = dogsScreen;
+        _africanelephantsScreen = africanelephantsScreen;
+        _polarBearsScreen = polarBearsScreen;
+        _chimmanzee = chimpanzeesScreen;
+    }
+   
+    #endregion Properties And Ctor
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public override void Show()
+    {
+        while (true)
+        {
+            Console.BackgroundColor = _settingsService.settings.MammalsScreen;
+            Console.WriteLine();
+            Console.WriteLine("Your available choices are:");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Dogs");
+            Console.WriteLine("2. African elephant");
+            Console.WriteLine("3. Polar bear");
+            Console.WriteLine("4. Chimpanzee");
+            Console.Write("Please enter your choice: ");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            string? choiceAsString = Console.ReadLine();
+
+            // Validate choice
+            try
+            {
+                if (choiceAsString is null)
+                {
+                    throw new ArgumentNullException(nameof(choiceAsString));
+                }
+
+                MammalsScreenChoices choice = (MammalsScreenChoices)Int32.Parse(choiceAsString);
+                switch (choice)
+                {
+                    case MammalsScreenChoices.Dogs:
+                        _dogsScreen.Show();
+                        break;
+                    case MammalsScreenChoices.Elephant:
+                        _africanelephantsScreen.Show(); 
+                        break;
+                    case MammalsScreenChoices.Bear:
+                        _polarBearsScreen.Show();
+                        break;
+                    case MammalsScreenChoices.Chimpanzee:
+                        _chimmanzee.Show();
+                        break;
+
+                    case MammalsScreenChoices.Exit:
+                        Console.WriteLine("Going back to parent menu.");
+                        return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    #endregion // Public Methods
+}
